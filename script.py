0d2458399286a5633988f731011ffba463fe91f2
Lars Dahlke <lars.dahlke@gmx.de>
2021-09-24 20:54:02 +0200

    Commented create issue out

--- script.py
+++ script.py
@@ -23,10 +23,11 @@ else:
 github = github.Github(token)
 # GITHUB_REPOSITORY is the repo name in owner/name format in Github Workflow
 repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
+print(os.environ['GITHUB_REPOSITORY'])
 
-issue = repo.create_issue(
-    title=title,
-    body=body,
-    assignees=assignees,
-    labels=labels
-)
+# issue = repo.create_issue(
+#     title=title,
+#     body=body,
+#     assignees=assignees,
+#     labels=labels
+# )
