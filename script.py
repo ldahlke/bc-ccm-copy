80a97d08a5f4225363de0ed8e0866fcf64705f67
Lars Dahlke <lars.dahlke@gmx.de>
2021-11-06 19:33:53 +0100

    Scheduled runs

--- script.py
+++ script.py
@@ -1,27 +1,6 @@
-import os
-import github
+import os, sys, time
 
-# extracting all the input from environments
-version = os.environ['INPUT_VERSION']
-tags = os.environ['INPUT_TAGS']
-token = os.environ['INPUT_TOKEN']
-labels = os.environ['INPUT_LABELS']
-target = os.environ['INPUT_TARGET']
-
-# as I said GitHub expects labels and assignees as list but we supplied as string in yaml as list are not supposed in
-# .yaml format
-if tags and tags != '':
-    tags = tags.split(',')  # splitting by , to make a list
-else:
-    tags = []  # setting empty list if we get labels as '' or None
-
-github = github.Github(token)
-# GITHUB_REPOSITORY is the repo name in owner/name format in Github Workflow
-repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
-repo.clone()    
-print(os.environ['GITHUB_REPOSITORY'])
-print(version)
-print(tags)
-print(labels)
-print(target)
-print(f"::set-output name=filescopied::{10}")
+runid = os.environ['INPUT_ID']
+for x in range(20):
+  print("Run number: {}, repeated {}".format(runid, x))
+  time.sleep(20)
